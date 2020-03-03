d0da77c3bef6fb38d329e328d6aae33fb6f70cc5
tudorgabriel24 <tudorgabriel1398@gmail.com>
2020-03-03 12:10:10 +0200

    init rating system

---
+++ Frontend/src/app/core/components/rating-system/rating-system.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-system',
+  templateUrl: './rating-system.component.html',
+  styleUrls: ['./rating-system.component.scss']
+})
+export class RatingSystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Frontend/src/app/core/core.module.ts
+++ Frontend/src/app/core/core.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent, FooterComponent } from './components';
+import { RatingSystemComponent } from './components/rating-system/rating-system.component';
 
 @NgModule({
   imports: [],
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, RatingSystemComponent],
 })
 export class CoreModule { }
